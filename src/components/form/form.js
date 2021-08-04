f46c750cb5f87fc001740283457cfe7db89ade7f
JimakPasha <Jimak@tut.by>
2021-08-04 23:45:01 +0300

    feat: validate form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,32 +1,78 @@
 import React, { useState, useEffect } from 'react';
+import SuccessMessage from '../successMessage/successMessage';
 import './form.scss';
 
 const Form = ({ setFormValues }) => {
-	const [firstName, setfirstName] = useState('');
+	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 	const [deliveryDate, setDeliveryDate] = useState('');
-	const [country, setCountry] = useState('Belarus');
+	const [country, setCountry] = useState('');
 	const [checkAgree, setCheckAgree] = useState(false);
 	const [checkNotice, setCheckNotice] = useState(false);
+	const [errors, setErrors] = useState({});
+
+	useEffect(()=> {
+		validate()
+	}, [firstName, lastName, deliveryDate, country, checkAgree])
+
+	const validate = () => {
+		setErrors({});
+		if (firstName === '') {
+			setErrors((state) => ({ ...state, firstName }))
+		}
+		if (lastName === '') {
+			setErrors((state) => ({ ...state, lastName }))
+		}
+		if (deliveryDate === '') {
+			setErrors((state) => ({ ...state, deliveryDate }))
+		}
+		if (country === '') {
+			setErrors((state) => ({ ...state, country }))
+		}
+		if (!checkAgree) {
+			setErrors((state) => ({ ...state, checkAgree }))
+		}
+	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		setFormValues((state) => [...state, { firstName, lastName, deliveryDate, country, checkAgree, checkNotice }]);
+		if (Object.keys(errors).length === 0) {
+			setFormValues((state) => [...state, { firstName, lastName, deliveryDate, country, checkAgree, checkNotice }]);
+			reset();
+			alert('Data saved successfully!')
+		} else {
+			alert('Fill in all the required fields please!')
+		}
 	}
+
+	const reset = () => {
+		setFirstName('');
+		setLastName('');
+		setDeliveryDate('');
+		setCountry('');
+		setCheckAgree(false);
+		setCheckNotice(false);
+		setErrors('');
+	}
+
 	return (
 		<form className="form" onSubmit={handleSubmit}>
 			<label htmlFor="firstName">
-				Name:
+				<p>
+					Name: {errors?.firstName === '' && <span className="erorr-validate">*name should be fill</span>}
+				</p>
 				<input
 					className="form__input"
 					type="text"
 					name="firstName"
 					value={firstName}
-					onChange={(e) => setfirstName(e.target.value)}
+					onChange={(e) => setFirstName(e.target.value)}
 				/>
 			</label>
 			<label htmlFor="lastName">
-				Surname:
+				<p>
+					Surname: {errors?.lastName === '' && <span className="erorr-validate">*surname should be fill</span>}
+				</p>
 				<input
 					className="form__input"
 					type="text"
@@ -36,7 +82,9 @@ const Form = ({ setFormValues }) => {
 				/>
 			</label>
 			<label htmlFor="deliveryDate">
-				Delivery date:
+				<p>
+					Delivery date: {errors?.deliveryDate === '' && <span className="erorr-validate">*date should be fill</span>}
+				</p>
 				<input
 					className="form__input"
 					type="date"
@@ -46,13 +94,16 @@ const Form = ({ setFormValues }) => {
 				/>
 			</label>
 			<label htmlFor="country">
-				Country:
+				<p>
+					Country: {errors?.country === '' && <span className="erorr-validate">*country should be fill</span>}
+				</p>
 				<select
 					className="form__select"
 					name="country"
 					value={country}
 					onChange={(e) => setCountry(e.target.value)}
 				>
+					<option></option>
 					<option>Belarus</option>
 					<option>USA</option>
 					<option>Canada</option>
@@ -63,12 +114,12 @@ const Form = ({ setFormValues }) => {
 				</select>
 			</label>
 			<label className="form__label-check" htmlFor="checkAgree">
-				<p className="form__check-text">I agree to have my personal data processed</p>
+				<p className="form__check-text">I agree to have my personal data processed {errors?.checkAgree !== undefined && <span className="erorr-validate">*agree should be check</span>}</p>
 				<input
 					className="form__input form__input-check"
 					type="checkbox"
 					name="checkAgree"
-					value={checkAgree}
+					checked={checkAgree}
 					onChange={() => setCheckAgree(prev => !prev)}
 				/>
 			</label>
@@ -78,7 +129,7 @@ const Form = ({ setFormValues }) => {
 					className="form__input form__input-check"
 					type="checkbox"
 					name="checkNotice"
-					value={checkNotice}
+					checked={checkNotice}
 					onChange={() => setCheckNotice(prev => !prev)}
 				/>
 			</label>
---
+++ src/components/successMessage/successMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './successMessage.scss';
+
+const SuccessMessage = () => {
+	return (
+		<div>
+			!!!!!
+		</div>
+	)
+}
+
+export default SuccessMessage;
