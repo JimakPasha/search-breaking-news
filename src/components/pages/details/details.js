b3bcbf2c9c4b72301a0097c4c575136d9d646088
JimakPasha <Jimak@tut.by>
2021-08-31 14:46:22 +0300

    fix: show loading in details component

--- src/components/pages/details/details.js
+++ src/components/pages/details/details.js
@@ -9,7 +9,6 @@ import './details.scss';
 const Details = () => {
 	const dispatch = useDispatch();
 	const params = useParams();
-	const news = useSelector((state) => state.search.dataDetails);
 	const loading = useSelector((state) => state.search.loading);
 	const error = useSelector((state) => state.search.error);
 
@@ -18,39 +17,48 @@ const Details = () => {
 		dispatch(getNewsDetails(params.title));
 	}, []);
 
+	const showError = error ? <Error /> : null;
+	const showloading = loading ? <Loading /> : null;
+	const content = !(loading || error) ? <View /> : null;
+
+
 	return (
 		<>
 			<div>
-				{error ? <Error /> : null}
-				{loading ? <Loading /> : null}
-				<div className="details">
-					<h3 className="details__title">{news.title}</h3>
-					<img
-						className="details__img"
-						src={news.urlToImage}
-						alt={news.title}
-					/>
-					<p className="details__content">{news.content}</p>
-					<div className="details__description">
-						<a
-							className="details__link"
-							href={news.url}
-							target="_blank"
-							rel="noreferrer"
-						>
-							Go to the official news source
-						</a>
-						<div className="details__author-wrapp">
-							<p className="details__author">Author: {news.author}</p>
-							<p className="details__data-published">
-								Published: {news.publishedAt}
-							</p>
-						</div>
-					</div>
-				</div>
+				{showError}
+				{showloading}
+				{content}
 			</div>
 		</>
 	);
 };
 
+const View = () => {
+	const news = useSelector((state) => state.search.dataDetails);
+	return (
+		<div className="details">
+			<h3 className="details__title">{news.title}</h3>
+			<img className="details__img" src={news.urlToImage} alt={news.title} />
+			<p className="details__content">{news.content}</p>
+			<div className="details__description">
+				<a
+					className="details__link"
+					href={news.url}
+					target="_blank"
+					rel="noreferrer"
+				>
+					Go to the official news source
+				</a>
+				<div className="details__author-wrapp">
+					<p className="details__author">Author: {news.author}</p>
+					<p className="details__data-published">
+						Published: {news.publishedAt}
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 export default Details;
+
