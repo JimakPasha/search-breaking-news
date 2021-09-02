d0c9d83d380827e2107ad2de9c1a70aa9c2fa1a2
JimakPasha <Jimak@tut.by>
2021-09-02 22:31:30 +0300

    feat: add router testing2

--- test/reacttest.test.jsx
+++ test/reacttest.test.jsx
@@ -35,120 +35,120 @@ describe('Redux testing:', () => {
 
 
 
-// describe('React router testing:', () => {
-// 	it('should render the home page', () => {
-// 		const history = createMemoryHistory();
-// 		const { container, getByText } = render(
-// 			<Provider store={store}>
-// 				<Router history={history}>
-// 					<Header />
-// 					<PanelForm />
-// 				</Router>
-// 			</Provider>
-// 		);
-// 		expect(container.innerHTML).toMatch('Relevancy');
-// 		expect(getByText(/Home/i));
-// 	})
-// 	it('should navigate to page about', () => {
-// 		const history = createMemoryHistory();
-// 		const { container } = render(
-// 			<Provider store={store}>
-// 				<Router history={history}>
-// 					<Header />
-// 					<PanelForm />
-// 					<About />
-// 				</Router>
-// 			</Provider>
-// 		);
-// 		fireEvent.click(screen.getByText('about'));
-// 		expect(container.innerHTML).toMatch('JimakPasha');
-// 	})
-// 	it('should navigate to error page if route is wrong', () => {
-// 		const history = createMemoryHistory();
-// 		history.push('wrong-route');
-// 		const { container } = render(
-// 			<Provider store={store}>
-// 				<Router history={history}>
-// 					<Header />
-// 					<ErrorPage />
-// 				</Router>
-// 			</Provider>
-// 		);
-// 		expect(container.innerHTML).toMatch('Oops... Error 404');
-// 	})
-// })
+describe('React router testing:', () => {
+	it('should render the home page', () => {
+		const history = createMemoryHistory();
+		const { container, getByText } = render(
+			<Provider store={store}>
+				<Router history={history}>
+					<Header />
+					<PanelForm />
+				</Router>
+			</Provider>
+		);
+		expect(container.innerHTML).toMatch('Relevancy');
+		expect(getByText(/Home/i));
+	})
+	it('should navigate to page about', () => {
+		const history = createMemoryHistory();
+		const { container } = render(
+			<Provider store={store}>
+				<Router history={history}>
+					<Header />
+					<PanelForm />
+					<About />
+				</Router>
+			</Provider>
+		);
+		fireEvent.click(screen.getByText('about'));
+		expect(container.innerHTML).toMatch('JimakPasha');
+	})
+	it('should navigate to error page if route is wrong', () => {
+		const history = createMemoryHistory();
+		history.push('wrong-route');
+		const { container } = render(
+			<Provider store={store}>
+				<Router history={history}>
+					<Header />
+					<ErrorPage />
+				</Router>
+			</Provider>
+		);
+		expect(container.innerHTML).toMatch('Oops... Error 404');
+	})
+})
 
-// describe('Error component testing:', () => {
-// 	it('should text:', () => {
-// 		render(<ErrorPage />);
-// 		expect(screen.getByText(/Oops... Error 404/i)).toBeInTheDocument();
-// 	})
-// 	it('should text:', () => {
-// 		render(<ErrorPage />);
-// 		expect(screen.getByText(/This page not found :/i)).toBeInTheDocument();
-// 	})
-// })
+describe('Error component testing:', () => {
+	it('should text:', () => {
+		render(<ErrorPage />);
+		expect(screen.getByText(/Oops... Error 404/i)).toBeInTheDocument();
+	})
+	it('should text:', () => {
+		render(<ErrorPage />);
+		expect(screen.getByText(/This page not found :/i)).toBeInTheDocument();
+	})
+})
 
-// describe('About component testing:', () => {
-// 	it('should text:', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/In this application, you can use the search bar and find the latest news./i)).toBeInTheDocument();
-// 		expect(screen.getByText(/In this application, you can use the search bar and find the latest news./i)).toHaveClass('about__text');
-// 	});
-// 	it('should title:', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/Since api is free, there are some features:/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/Since api is free, there are some features:/i)).toHaveClass('about__peculiarities-title');
-// 	});
-// 	it('should peculiarities-item:', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/- Returns only the first 100 news -/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/- Returns only the first 100 news -/i)).toHaveClass('about__peculiarities-item');
-// 	});
-// 	it('should peculiarities-item:', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/- You can make 100 requests within 12 hours -/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/- You can make 100 requests within 12 hours -/i)).toHaveClass('about__peculiarities-item');
-// 	});
-// 	it('should peculiarities-item:', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/- On the details-page, the content of the article is incomplete -/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/- On the details-page, the content of the article is incomplete -/i)).toHaveClass('about__peculiarities-item');
-// 	});
-// 	it('should text before gihub', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toHaveClass('about__text');
-// 	});
-// 	it('gihub', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/gihub/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/gihub/i)).toHaveClass('about__link');
-// 	});
-// 	it('should text thanks', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/Thanks to for providing the api./i)).toBeInTheDocument();
-// 		expect(screen.getByText(/Thanks to for providing the api./i)).toHaveClass('about__text');
-// 	});
-// 	it('thank you to whom', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/newsapi/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/newsapi/i)).toHaveClass('about__link');
-// 	});
-// 	it('should text who made app', () => {
-// 		render(<About />);
-// 		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toBeInTheDocument();
-// 		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toHaveClass('about__text');
-// 	});
-// 	it('should role link', () => {
-// 		render(<About />);
-// 		expect(screen.getAllByRole('link')[0]).toBeInTheDocument();
-// 	})
-// 	it('should role link', () => {
-// 		render(<About />);
-// 		expect(screen.getAllByRole('link')[1]).toBeInTheDocument();
-// 	})
-// });
+describe('About component testing:', () => {
+	it('should text:', () => {
+		render(<About />);
+		expect(screen.getByText(/In this application, you can use the search bar and find the latest news./i)).toBeInTheDocument();
+		expect(screen.getByText(/In this application, you can use the search bar and find the latest news./i)).toHaveClass('about__text');
+	});
+	it('should title:', () => {
+		render(<About />);
+		expect(screen.getByText(/Since api is free, there are some features:/i)).toBeInTheDocument();
+		expect(screen.getByText(/Since api is free, there are some features:/i)).toHaveClass('about__peculiarities-title');
+	});
+	it('should peculiarities-item:', () => {
+		render(<About />);
+		expect(screen.getByText(/- Returns only the first 100 news -/i)).toBeInTheDocument();
+		expect(screen.getByText(/- Returns only the first 100 news -/i)).toHaveClass('about__peculiarities-item');
+	});
+	it('should peculiarities-item:', () => {
+		render(<About />);
+		expect(screen.getByText(/- You can make 100 requests within 12 hours -/i)).toBeInTheDocument();
+		expect(screen.getByText(/- You can make 100 requests within 12 hours -/i)).toHaveClass('about__peculiarities-item');
+	});
+	it('should peculiarities-item:', () => {
+		render(<About />);
+		expect(screen.getByText(/- On the details-page, the content of the article is incomplete -/i)).toBeInTheDocument();
+		expect(screen.getByText(/- On the details-page, the content of the article is incomplete -/i)).toHaveClass('about__peculiarities-item');
+	});
+	it('should text before gihub', () => {
+		render(<About />);
+		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toBeInTheDocument();
+		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toHaveClass('about__text');
+	});
+	it('gihub', () => {
+		render(<About />);
+		expect(screen.getByText(/gihub/i)).toBeInTheDocument();
+		expect(screen.getByText(/gihub/i)).toHaveClass('about__link');
+	});
+	it('should text thanks', () => {
+		render(<About />);
+		expect(screen.getByText(/Thanks to for providing the api./i)).toBeInTheDocument();
+		expect(screen.getByText(/Thanks to for providing the api./i)).toHaveClass('about__text');
+	});
+	it('thank you to whom', () => {
+		render(<About />);
+		expect(screen.getByText(/newsapi/i)).toBeInTheDocument();
+		expect(screen.getByText(/newsapi/i)).toHaveClass('about__link');
+	});
+	it('should text who made app', () => {
+		render(<About />);
+		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toBeInTheDocument();
+		expect(screen.getByText(/This application is made by JimakPasha. My/i)).toHaveClass('about__text');
+	});
+	it('should role link', () => {
+		render(<About />);
+		expect(screen.getAllByRole('link')[0]).toBeInTheDocument();
+	})
+	it('should role link', () => {
+		render(<About />);
+		expect(screen.getAllByRole('link')[1]).toBeInTheDocument();
+	})
+});
 
 
 
